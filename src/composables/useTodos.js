209e1a1e2c001c3092e463ba8a154cd3902f6242
YuelinWang <836473734@qq.com>
2020-12-07 22:06:45 +0800

    read

--- src/composables/useTodos.js
+++ src/composables/useTodos.js
@@ -1,29 +1,26 @@
 import { onMounted, ref } from "vue";
 
 export default function useTodos() {
-  const todos = ref([]);
-  // 添加 todo
-  const addTodo = (todo) => todos.value.push(todo);
+    const todos = ref([]);
+    // 添加 todo
+    const addTodo = (todo) => todos.value.push(todo);
 
-  // 获取远程 todos
-  const fetchTodos = async () => {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/todos?_limit=5"
-    );
-    const rawTodos = await response.json();
-    todos.value = rawTodos.map((todo) => ({
-      id: todo.id,
-      content: todo.title,
-      completed: todo.completed,
-    }));
-  };
+    // 获取远程 todos
+    const fetchTodos = async() => {
+        let data = await window.android.getNotesJson();
+        todos.value = data.map((todo) => ({
+            id: todo.id,
+            content: todo.title,
+            completed: todo.completed,
+        }));
+    };
 
-  onMounted(() => {
-    fetchTodos();
-  });
+    onMounted(() => {
+        fetchTodos();
+    });
 
-  return {
-    todos,
-    addTodo,
-  };
+    return {
+        todos,
+        addTodo,
+    };
 }
